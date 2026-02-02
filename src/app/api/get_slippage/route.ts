2faf3051c1d613ec61c57b0ddd23a2e8e931a3ab
zillerium <sales@merrows.co.uk>
2026-02-02 15:29:51 +0000

    fetch

--- src/app/api/get_slippage/route.ts
+++ src/app/api/get_slippage/route.ts
@@ -1,7 +1,17 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  const res = await fetch("https://decoswap.xyz/api/get_slippage", {
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+
+  const amount = searchParams.get("amount");
+  const token1 = searchParams.get("token1");
+  const token2 = searchParams.get("token2");
+
+  const upstreamUrl =
+    `https://decoswap.xyz/api/get_slippage` +
+    `?amount=${amount}&token1=${token1}&token2=${token2}`;
+
+  const res = await fetch(upstreamUrl, {
     cache: "no-store",
   });
 
